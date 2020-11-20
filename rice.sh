a32a3dd536494518e157443606df5a0b12136c79
Stear <Stear@MacBook.local>
2020-11-20 14:10:05 +0100

    correction des versions de pakages

--- rice.sh
+++ rice.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
 GREEN='\033[0;32m'
+MAGEN='\e[95m'
 RESET='\033[0m'
 
 # Mise a jour
 dnf upgrade -y
+echo "${MAGEN}--- Mises a jour terminee ---${RESET}"
 sleep 10
 
+# Installation des repertoires RPM fusion 
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Installation des packages
-echo -e "${GREEN}--- Installation de i3 et i3-blocks ---${RESET}"
-dnf install i3 i3blocks -y
+echo -e "${MAGEN}--- Installation de i3 ---${RESET}"
+dnf install i3 -y
 
-echo -e "${GREEN}--- Installation des packages de base ---${RESET}"
-dnf install vim nmap keepassxc tmux zathura vlc virtualbox -y
+echo -e "${MAGEN}--- Installation des packages de base ---${RESET}"
+dnf install vim nmap.x86_64 keepassxc.x86_64 tmux -y
+dnf install zathura.x86_64  zathura-pdf-mupdf.x86_64 -y
+dnf install vlc VirtualBox.x86_64  -y
 
 # Installation de vscode
-echo -e "${GREEN}--- Installation de vscode ---${RESET}"
+echo -e "${MAGEN}--- Installation de vscode ---${RESET}"
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 dnf check-update && dnf install code -y
 
 # Installation de Metasploit
-echo -e "${GREEN}--- Installation de metasploit ---${RESET}"
+echo -e "${MAGEN}--- Installation de metasploit ---${RESET}"
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
   chmod 755 msfinstall && \
   ./msfinstall
 
 # Configuration de zsh
-dnf install chsh -y 
+dnf install util-linux-user -y
 dnf install zsh -y
 if type -p zsh > /dev/null; then
     echo "zsh installe"
@@ -38,3 +44,8 @@ else
 fi
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Rappel 
+echo -e "${MAGEN}--- /!\ Installer burpsuite /!\ ---${MAGEN}"
+echo -e "${MAGEN}--- /!\ Installer gobuster /!\ ---${MAGEN}"
+echo -e "${MAGEN}--- /!\ Installer dictionnaires seclists /!\ ---${MAGEN}"
