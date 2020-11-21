a0f26287c6c47c98299a81f7b5c74407943b66f2
Stear <Stear@MacBook.local>
2020-11-21 18:32:44 +0100

    corrections

--- rice.sh
+++ rice.sh
@@ -23,12 +23,21 @@ cd i3blocks
 ./configure
 make
 make install
+sleep 10 
 
+
+# Installation des packages 
 echo -e "${MAGEN}--- Installation des packages de base ---${RESET}"
 dnf install vim nmap.x86_64 keepassxc.x86_64 tmux -y
+dnf install texlive-scheme-full -y
+dnf install fontawesome-fonts.noarch -y
 dnf install lxappearance.x86_64 arc-theme -y 
 dnf install zathura.x86_64  zathura-pdf-mupdf.x86_64 -y
+dnf install pandoc -y
 dnf install vlc VirtualBox.x86_64  -y
+echo -e "${MAGEN}--- Installation des packages de base terminee ---${RESET}"
+sleep 10
+
 
 # Installation de vscode
 echo -e "${MAGEN}--- Installation de vscode ---${RESET}"
@@ -36,12 +45,13 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 dnf check-update && dnf install code -y
+sleep 10
 
 # Installation de Metasploit
 echo -e "${MAGEN}--- Installation de metasploit ---${RESET}"
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
-  chmod 755 msfinstall && \
-  ./msfinstall
+chmod 755 msfinstall && \
+./msfinstall
 
 # Configuration de zsh
 dnf install util-linux-user -y
